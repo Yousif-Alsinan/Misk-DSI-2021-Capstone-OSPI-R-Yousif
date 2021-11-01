91e468591bc052448c4e3ce6320425aebacbf229
Yousif Alsinan <88483242+Yousif-Alsinan@users.noreply.github.com>
2021-11-01 15:12:34 -0400

    Third commit

--- DSI-Capstone-R-Script.R
+++ DSI-Capstone-R-Script.R
@@ -1,13 +1,16 @@
 
+# Loading data
 library(readr)
 ospi <- read_csv("~/GitHub/Misk-DSI-CS-OSPI-Yousif Alsinan/online_shoppers_intention.csv")
 
+#Loading libraries
 library(dplyr)
 library(explore)
 library(ggplot2)
 library(tidyverse)
 library(caret)
 
+# Getting familiar with the data
 ospi %>%
   explore_all()
 
@@ -33,3 +36,115 @@ summary(ospi$RevenueBi)
 colSums(is.na(ospi))
 
 colSums(ospi == "")
+
+# Exploratory Data Analysis
+
+# 1. Numrical
+
+summary(ospi$Administrative)
+
+ospi %>% 
+  ggplot() +
+  aes(x = Administrative) +
+  geom_bar() +
+  facet_grid(Revenue ~ .,
+    scales = "free_y")
+
+summary(ospi$Administrative_Duration)
+
+ospi %>% 
+  ggplot() +
+  aes(x = Administrative_Duration) +
+  geom_histogram(bins = 50) +
+  facet_grid(Revenue ~ .,
+    scales = "free_y")
+
+summary(ospi$Informational)
+
+ospi %>% 
+  ggplot() +
+  aes(x = Informational) +
+  geom_bar() +
+  facet_grid(Revenue ~ .,
+    scales = "free_y")
+
+summary(ospi$Informational_Duration)
+
+ospi %>% 
+  ggplot() +
+  aes(x = Informational_Duration) +
+  geom_histogram(bins = 50) +
+  facet_grid(Revenue ~ .,
+    scales = "free_y")
+
+summary(ospi$ProductRelated)
+
+ospi %>% 
+  ggplot() +
+  aes(x = ProductRelated) +
+  geom_bar() +
+  facet_grid(Revenue ~ .,
+    scales = "free_y")
+
+summary(ospi$ProductRelated_Duration)
+
+ospi %>% 
+  ggplot() +
+  aes(x = ProductRelated_Duration) +
+  geom_histogram(bins = 100) +
+  facet_grid(Revenue ~ .,
+    scales = "free_y")
+
+summary(ospi$BounceRates)
+
+ospi %>% 
+  ggplot() +
+  aes(x = BounceRates) +
+  geom_histogram(bins = 100) +
+  facet_grid(Revenue ~ .,
+    scales = "free_y")
+
+summary(ospi$ExitRates)
+
+ospi %>% 
+  ggplot() +
+  aes(x = ExitRates) +
+  geom_histogram(bins = 100) +
+  facet_grid(Revenue ~ .,
+    scales = "free_y")
+
+summary(ospi$PageValues)
+
+ospi %>% 
+  ggplot() +
+  aes(x = PageValues) +
+  geom_histogram(bins = 50) +
+  facet_grid(Revenue ~ .,
+    scales = "free_y")
+
+summary(ospi$SpecialDay)
+
+ospi %>% 
+  ggplot() +
+  aes(x = SpecialDay) +
+  geom_bar() +
+  facet_grid(Revenue ~ .,
+    scales = "free_y")
+
+# 2. Categorical
+
+ospi %>% 
+  ggplot() +
+  aes(x = Month, Revenue = ..count../nrow(ospi), fill = Revenue) +
+  geom_bar() + 
+  ylab("relative frequency")
+
+month_table <- table(ospi$Month, ospi$Revenue)
+month_tab <- as.data.frame(prop.table(month_table, 2))
+colnames(month_tab) <-  c("Month", "Revenue", "perc")
+
+ggplot(data = month_tab, aes(x = Month, y = perc, fill = Revenue)) + 
+  geom_bar(stat = 'identity', position = 'dodge', alpha = 2/3) + 
+  xlab("Month")+
+  ylab("Percent")
+
